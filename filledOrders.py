4ee487ce72bccdbf87332fd0d51ac9c1d3ac23fa
Shreya Hegde <106897123+shreyahegde18@users.noreply.github.com>
2022-11-11 17:29:20 +0530

    Add files via upload

---
+++ filledOrders.py
@@ -0,0 +1,51 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+#
+# Complete the 'filledOrders' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY order
+#  2. INTEGER k
+#
+
+def filledOrders(order, k):
+    # Write your code here
+    
+    order.sort()
+    c=0
+    for i in range(len(order)):
+        if k>0:
+            x=order[i]
+            k=k-x
+            if k>=0:
+           
+             c=c+1
+    return c       
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    order_count = int(input().strip())
+
+    order = []
+
+    for _ in range(order_count):
+        order_item = int(input().strip())
+        order.append(order_item)
+
+    k = int(input().strip())
+
+    result = filledOrders(order, k)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
